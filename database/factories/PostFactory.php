63b74f5b70ab785e7a3e993101764dc0c952b2ad
Shahad1994 <ody.ibraheem@gmail.com>
2019-09-18 11:02:07 +0300

    Update PostFactory.php

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -10,11 +10,8 @@ $factory->define(Post::class, function (Faker $faker) {
     return [
          'title' => $faker->word,
          'content' => $faker->text,
-<<<<<<< HEAD
-         'image' => $faker->'http://lorempixel.com/800/600/cats/' ,
-=======
-         'image' =>  $faker->'http://lorempixel.com/800/600/cats/',
->>>>>>> 1824ce499bfd4a76896efa6c1ecd3aef2d11902c
+         'image' => asset("logo.png"),
+
          'user_id' => function() {
          	return factory(App\User::class)->create()->id;
          }
